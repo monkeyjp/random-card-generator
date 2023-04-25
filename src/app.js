5c8f6e5d0dbeb3b5b36d53217dd23e47eed6b072
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2023-04-25 20:54:24 +0000

    add Fuction onLoad to show random card

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let randomCardValue = Math.floor(Math.random() * 13);
+  const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k", "A"];
+  let randomCardSuits = Math.floor(Math.random() * 4);
+  const cardSuits = ["♦", "♥", "♠", "♣"];
+  let cardValue = document.querySelector(".cardValue");
+  cardValue.innerHTML = cardValues[randomCardValue];
+  let cardSuit = document.querySelectorAll("#suits");
+  for (let i = 0; i < cardSuit.length; i++) {
+    cardSuit[i].classList.add(cardSuits[randomCardSuits]);
+    cardSuit[i].innerHTML = cardSuits[randomCardSuits];
+  }
 };
